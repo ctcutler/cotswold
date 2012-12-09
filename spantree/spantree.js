aa3f039089cc8c378a1f72ac2a95e49a3b1fb84b
ctcutler <ctcutler>
2012-12-09 12:59:19 -0600

    Beginning to implement more general case of nesting and normal spans.

--- spantree/spantree.js
+++ spantree/spantree.js
@@ -75,6 +75,7 @@ controller("TestController", ['$scope', function($scope) {
     { start: 0, end: 11, id: "range1", style: "red" },
     { start: 18, end: 21, id: "range2", style: "blue" },
 
+    /*
     // nested (three layers)
     { start: 23, end: 38, id: "range3", style: "red" },
     { start: 26, end: 34, id: "range4", style: "blue" },
@@ -85,23 +86,26 @@ controller("TestController", ['$scope', function($scope) {
     { start: 13, end: 14, id: "nest2", style: "blue" },
     { start: 15, end: 16, id: "nest3", style: "green" },
 
+    // nested, matching start, shorter first 
+    { start: 40, end: 43, id: "range6", style: "blue" },
+    { start: 40, end: 44, id: "range7", style: "red" },
+
     // nested, matching start, longer first
     { start: 50, end: 54, id: "range8", style: "red" },
     { start: 50, end: 53, id: "range9", style: "blue" },
 
+    // nested, matching end, shorter first 
+    { start: 61, end: 64, id: "range10", style: "blue" },
+    { start: 60, end: 64, id: "range11", style: "red" },
+
     // nested, matching end, longer first
-    { start: 70, end: 74, id: "range6", style: "red" },
-    { start: 71, end: 74, id: "range7", style: "blue" },
+    { start: 70, end: 74, id: "range12", style: "red" },
+    { start: 71, end: 74, id: "range13", style: "blue" },
 
-    /*
+    // nested, shorter first 
+    { start: 76, end: 77, id: "range14", style: "blue" },
+    { start: 75, end: 78, id: "range15", style: "red" },
 
-    // nested, matching start, shorter first (effectively an overlap)
-    { start: 40, end: 43, id: "range6", style: "blue" },
-    { start: 40, end: 44, id: "range7", style: "red" },
-
-    // nested, matching end, shorter first (effectively an overlap)
-    { start: 61, end: 64, id: "range6", style: "blue" },
-    { start: 60, end: 64, id: "range7", style: "red" },
 
 
     // overlapping
@@ -235,7 +239,7 @@ controller("TestController", ['$scope', function($scope) {
     
   $scope.loadSpanTree2 = function () {
     var contentNodes = [makeContentNode(0, $scope.content.length)]; 
-    var tree = makeSpanNode("", false, "", [contentNodes[0]]); // must not be contentNodes array
+    var tree = makeSpanNode("", false, "", [contentNodes[0]], null); // must not be contentNodes array
 
     angular.forEach($scope.ranges, function(range) {
       var nodes = [];
@@ -250,6 +254,7 @@ controller("TestController", ['$scope', function($scope) {
         console.log("range from "+range.start+" to "+range.end+" didn't match any nodes");
       } 
 
+      /*
       // one node so range must be nested within it
       else if (nodes.length == 1) {
         // FIXME: filter out zero length content nodes
@@ -262,15 +267,163 @@ controller("TestController", ['$scope', function($scope) {
         replaceInArray(parentNode.nodes, contentNode, [leftContentNode, spanNode, rightContentNode]);
         replaceInArray(contentNodes, contentNode, [leftContentNode, rangeContentNode, rightContentNode]);
       }
+      */
+
+      else {
+        // find least common ancestor of nodes
+        var lca = findLCA(nodes);
+        var leftEdge = nodes[0];
+        var rightEdge = nodes[nodes.length-1];
+        var innerChildren = nodes.slice(1, nodes.length-1);
+
+        if (leftEdge == rightEdge) {
+          // make perfect content node for range and put in innerChildren
+          var innerChild = makeContentNode(range.start, range.end, leftEdge.parentNode);
+          innerChildren.push(innerChild);
+          leftEdge.parentNode.nodes.push(innerChild);
+          // create right edge node and add it to its parent
+          var rightEdge = makeContentNode(range.end, leftEdge.end, leftEdge.parentNode);
+          rightEdge.parentNode.nodes.push(rightEdge);
+          // update left edge node
+          updateContentNode(leftEdge, leftEdge.start, range.start);
+
+          // update content nodes
+          replaceObjInArray(contentNodes, leftEdge, [leftEdge, innerChild, rightEdge]);
+
+          // ignore the tweaked leftEdge and rightEdge
+          rightEdge = null;
+          leftEdge = null;
+        } 
+
+        if (leftEdge && leftEdge.start == range.start) {
+          innerChildren.unshift(leftEdge);
+          leftEdge = null;
+        }
+        if (rightEdge && rightEdge.end == range.end) {
+          innerChildren.push(rightEdge);
+          rightEdge = null;
+        }
+
+        // if edges' immediate parent is that common parent or edges match range 
+        // boundaries, this is the non-overlap case
+        if ((leftEdge == null || leftEdge.parentNode == lca)
+            && (rightEdge == null || rightEdge.parentNode == lca)) {
+
+          // make new span node children from nodes
+          var spanChildren = [];
+          var newContentNodes = [];
+          var lcaInnerChildren = getLCAChildren(lca, innerChildren);
+
+
+          // left edge
+          if (leftEdge) {
+            // make new node and add to span children, trim existing left edge
+            var newContentNode = makeContentNode(range.start, leftEdge.end);
+            spanChildren.push(newContentNode);
+            newContentNodes.push(newContentNode);
+            updateContentNode(leftEdge, leftEdge.start, range.start);
+          }
+
+          // inner
+          if (innerChildren.length > 0) {
+            newContentNodes.push.apply(newContentNodes, innerChildren);
+            spanChildren.push.apply(spanChildren, lcaInnerChildren);
+          }
+
+          // right edge
+          if (rightEdge) {
+            // make new node and add to span children, trim existing left edge
+            var newContentNode = makeContentNode(rightEdge.start, range.end);
+            spanChildren.push(newContentNode);
+            newContentNodes.push(newContentNode);
+            updateContentNode(rightEdge, range.end, rightEdge.end);
+          }
+
+          // make new span node
+          var spanNode = makeSpanNode(range.id, range.dominant, range.style, spanChildren);
+
+          // update lca (tree)
+          replaceArrayInArray(lca.nodes, lcaInnerChildren, [spanNode]);
+          // update contentNodes list (need to build list to update it with)
+          replaceArrayInArray(contentNodes, innerChildren, newContentNodes);
+        }
+      }
+  
     });
     
     $scope.spanTree.push(tree);
   };
 
-  function replaceInArray(arr, objToReplace, replacements) {
-    var args = [arr.indexOf(objToReplace), 1];
+  // My own crappy LCA algorithm, optimal only in the sense that I undestand it
+  function findLCA(nodes) {
+    var ancestors = []; // should make this a hash/map so lookups are efficient
+    var n = nodes[0];
+
+    // make list of ancestors of first node
+    while (n.parentNode != null) {
+      ancestors.push(n.parentNode);
+      n = n.parentNode;
+    }
+    ancestors.reverse();
+    var lcaIndex = ancestors.length-1;
+
+    // for every other node 
+    angular.forEach(nodes.slice(1), function (n) {
+      // move root-wards checking if each parent is in the ancestor list
+      while (n.parentNode != null) {
+        var idx = ancestors.indexOf(n.parentNode);
+        // if it is and it is higher in the ancestor list than the current LCA, update the LCA
+        if (idx != -1) {
+          lcaIndex = Math.max(lcaIndex, idx);
+          break;
+        }
+        n = n.parentNode;
+      }
+    });
+
+    if (lcaIndex == -1) {
+      return null;
+    } else {
+      return ancestors[lcaIndex];
+    }
+  }
+
+  // returns a list of lca's child nodes that are the ancestors of descendants
+  // most likely lots of opportunities for optimization here
+  function getLCAChildren(lca, descendants) {
+    var children = [];
+    angular.forEach(descendants, function (descendant) {
+      var n = descendant;
+      while (n.parentNode != null) {
+        if (n.parentNode == lca) {
+          children.push(n);
+          break;
+        }
+        n = n.parentNode;
+      }
+    });
+    return children;
+  }
+
+  function replaceObjInArray(arr, objToReplace, replacements) {
+    replaceItemsInArray(arr, arr.indexOf(objToReplace), 1, replacements);
+  }
+
+  function replaceArrayInArray(arr, arrToReplace, replacements) {
+    // assumes arrToReplace is a consecutive subset of arr
+    replaceItemsInArray(arr, arr.indexOf(arrToReplace[0]), arrToReplace.length, replacements);
+  }
+
+  function replaceItemsInArray(arr, index, count, replacements) {
+    var args = [index, count];
     args = args.concat(replacements);
     arr.splice.apply(arr, args);
+  }
+
+  function updateContentNode(node, start, end) {
+    node.start = start;
+    node.end = end;
+    node.content = $scope.content.substring(start, end);
   }
 
   function makeContentNode(start, end, parentNode) {
@@ -282,11 +435,12 @@ controller("TestController", ['$scope', function($scope) {
     };
   }
 
-  function makeSpanNode(id, dominant, style, nodes) {
+  function makeSpanNode(id, dominant, style, nodes, parentNode) {
     var spanNode = {
         dominant: dominant,
         style: style,
         nodes: nodes,
+        parentNode: parentNode
     };
     angular.forEach(nodes, function(node) {
       node.parentNode = spanNode;
