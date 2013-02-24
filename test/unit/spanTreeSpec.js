93672b21124fee9a20b9774fd7113f8e44e97869
ctcutler <ctcutler>
2013-02-24 17:59:59 -0600

    Write a bunch of empty test specs for the spantree library.

--- test/unit/spanTreeSpec.js
+++ test/unit/spanTreeSpec.js
@@ -1,9 +1,54 @@
 "use strict";
 
-describe("This test spec", function () {
+describe("The makeSpanTree() method", function() {
 
-  it("should confirm testacular and jasmine work.", function () {
-    expect(true).toBe(true);
+  it("should correctly represent distinct ranges.", function() {
+  });
+
+  it("should correctly represent nested ranges.", function() {
+  });
+
+  it("should correctly represent triple nesting.", function() {
+  });
+
+  it("should correctly represent two ranges separately nested in a third.", function() {
+  });
+
+  describe("should correctly represent nested ranges where they both have the same start", function() {
+
+    describe("where they both have the same start", function() {
+
+      it("and the shorter one comes first.", function() {
+      });
+
+      it("and the longer one comes first.", function() {
+      });
+
+    });
+
+    describe("where they both have the same end", function() {
+
+      it("and the shorter one comes first.", function() {
+      });
+
+      it("and the longer one comes first.", function() {
+      });
+
+    });
+
+  });
+
+  describe("should correctly represent overlapping ranges", function() {
+    it("where the top range comes first", function() {
+    });
+    it("where the top range comes last", function() {
+    });
+  });
+
+  it("should correctly represent adjacent ranges.", function() {
+  });
+
+  it("should correclty represent selected ranges.", function() {
   });
 
 });
