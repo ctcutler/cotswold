2fee529c4b42bfef25e5580de160b192cb6c64d2
ctcutler <ctcutler@gmail.com>
2013-03-17 15:54:28 -0500

    Add new directory with d3 proof of concept.

--- app/js/constants.js
+++ app/js/constants.js
@@ -6,5 +6,8 @@ var ARTIFACT_MAX_HEIGHT_EXPANDED = "none";
 // the left and top values reported by jquery's offset()
 // are off by this quantity. . . I can't explain why
 // but it makes me a little nervous
+// FIXME: check to see if the paper is offset by this much
+// FIXME: clear the default padding, border, etc. in the style
+//        sheet and this should go away (I hope)
 var MAGIC_OFFSET_CONSTANT = 5;
 
---
+++ d3/data.js
@@ -0,0 +1,86 @@
+// make this more like what spantree prints out. . . not literal ranges but instead
+// nested chunks of content, some with span ids
+
+var nested1 = [
+  {"start":0,"end":1,"content":"F"},
+  {
+    "class":"red allborders",
+    "nodes": [
+      {"start":1,"end":3,"content":"oo"},
+      {
+        "class":"blue allborders",
+        "nodes": [
+          {"start":3,"end":5,"content":" b"},
+          {
+            "class":"green allborders",
+            "nodes":[
+              {"start":5,"end":6,"content":"a"}
+            ],
+            "id":"range3"
+          },
+          {"start":6,"end":8,"content":"r "}
+        ],
+        "id":"range2"
+      },
+      {"start":8,"end":10,"content":"ba"}
+    ],
+    "id":"range1"
+  },
+  {"start":10,"end":11,"content":"z"}
+];
+
+var nested2 = [
+  {"start":0,"end":1,"content":"F"},
+  {
+    "class":"red allborders",
+    "nodes": [
+      {"start":1,"end":3,"content":"oo"},
+      {
+        "class":"blue allborders",
+        "nodes": [
+          {"start":3,"end":5,"content":" b"},
+          {
+            "class":"green allborders",
+            "nodes":[
+              {"start":5,"end":6,"content":"a"}
+            ],
+            "id":"range6"
+          },
+          {"start":6,"end":8,"content":"r "}
+        ],
+        "id":"range5"
+      },
+      {"start":8,"end":10,"content":"ba"}
+    ],
+    "id":"range4"
+  },
+  {"start":10,"end":11,"content":"z"}
+];
+
+var simple1 = [
+  {"content":"Foo bar baz"},
+];
+
+var timeline = [
+  [
+    {
+      nodes: nested1,
+    },
+    {
+      nodes: simple1,
+    },
+  ],
+  [
+    {
+      nodes: nested2,
+    },
+  ],
+];
+
+var connectionEnds = [
+  "range1", "range6" 
+];
+
+var connections = [
+  [ "range3", "range6"],
+];
